2cc1b2eefec71218d3b2d649a3bf6298e81af37e
SquashPrince <wowns1450@naver.com>
2026-08-27 17:22:29 +0900

    [생성] README, Program.cs

---
+++ Develrocket_mp15_Team_Assignment/Develrocket_mp15_Team_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=== 5조 팀원 소개 ===");
+
+        Console.WriteLine("=== # 5조 ## 김재준 ===");
+        Console.WriteLine("=== # 5조 ## 조재환 ===");
+        Console.WriteLine("=== # 5조 ## 유희찬 ===");
+
+        Console.WriteLine("=== 소개 끝 ===");
+    }
+
+    // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+}
