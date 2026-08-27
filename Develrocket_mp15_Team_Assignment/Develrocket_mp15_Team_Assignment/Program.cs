f741d8b2e7ad5fa34cb02d40a7139b87cc1c028e
bichen <alj2664@gmail.com>
2026-08-27 17:41:24 +0900

    [유희찬]/[소개 메서드 추가]

--- Develrocket_mp15_Team_Assignment/Develrocket_mp15_Team_Assignment/Program.cs
+++ Develrocket_mp15_Team_Assignment/Develrocket_mp15_Team_Assignment/Program.cs
@@ -10,8 +10,15 @@ public class Program
         Console.WriteLine("=== # 5조 ## 조재환 ===");
         Console.WriteLine("=== # 5조 ## 유희찬 ===");
 
+        bicheonganPrint();
+
         Console.WriteLine("=== 소개 끝 ===");
     }
 
     // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+
+    public static void bicheonganPrint()
+    {
+        Console.WriteLine("유희찬/ 5조의 팀원 유희찬 입니다. ");
+    }
 }
