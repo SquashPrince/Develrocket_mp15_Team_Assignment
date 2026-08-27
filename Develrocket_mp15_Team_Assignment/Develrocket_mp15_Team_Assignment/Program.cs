248cbc4e139b189570dc5a7747a935dd26d20cf0
bicheongan <alj2664@gmail.com>
2026-08-27 18:04:38 +0900

    Update Program.cs

--- Develrocket_mp15_Team_Assignment/Develrocket_mp15_Team_Assignment/Program.cs
+++ Develrocket_mp15_Team_Assignment/Develrocket_mp15_Team_Assignment/Program.cs
@@ -6,10 +6,9 @@ public class Program
     {
         Console.WriteLine("=== 5조 팀원 소개 ===");
 
-        IntroduceSquashPrince();
-        Console.WriteLine("=== # 5조 ## 조재환 ===");
+
         IntroSample();
-        Console.WriteLine("=== # 5조 ## 유희찬 ===");
+
 
         Console.WriteLine("=== 소개 끝 ===");
     }
@@ -19,14 +18,9 @@ public class Program
     public static void IntroSample()
     {
         Console.WriteLine("조재환 / 게임 방송을 좋아하니다");
+        Console.WriteLine("김재준 / 안녕하세요. 5조 팀장을 맡은 김재준이라고 합니다. 잘부탁드립니다.");
     }
     
 
-
-    public static void IntroduceSquashPrince()
-    {
-        Console.WriteLine("김재준 / 안녕하세요. 5조 팀장을 맡은 김재준이라고 합니다. 잘부탁드립니다.");
-
-    }
 
 }
