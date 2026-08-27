c111c23a21b80d811ad3846cf4707fe9a6eed9c6
woghks62 <p2pee@naver.com>
2026-08-27 17:33:15 +0900

    [Feat] 자기소개 메서드 추가 및 호출
    Closes #2

--- Develrocket_mp15_Team_Assignment/Develrocket_mp15_Team_Assignment/Program.cs
+++ Develrocket_mp15_Team_Assignment/Develrocket_mp15_Team_Assignment/Program.cs
@@ -8,10 +8,16 @@ public class Program
 
         Console.WriteLine("=== # 5조 ## 김재준 ===");
         Console.WriteLine("=== # 5조 ## 조재환 ===");
+        IntroSample();
         Console.WriteLine("=== # 5조 ## 유희찬 ===");
 
         Console.WriteLine("=== 소개 끝 ===");
     }
 
     // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    public static void IntroSample()
+    {
+        Console.WriteLine("조재환 / 게임 방송을 좋아하니다");
+    }
+    
 }
